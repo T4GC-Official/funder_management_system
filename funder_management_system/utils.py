cf1517f1548c1260cb4b8a627f8ba5d6b8ca41d6
neuron <praveen.902012@gmail.com>
2025-03-03 14:59:42 +0530

    Mod[Funder Management Workspace, Hooks, utils]: 1. Added cards to funder_management workspace. 2. Included custom_toolbar.js in hooks 3. Modified create_financial_year function to generate 5 previous financial years and 5 future financial years

--- funder_management_system/utils.py
+++ funder_management_system/utils.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 def create_financial_year():
     current_year = datetime.now().year
-    start_year = current_year - 10
-    end_year = current_year + 10
+    start_year = current_year - 5
+    end_year = current_year + 5
 
     for year in range(start_year, end_year + 1):
         financial_year = f"{year}-{str(year + 1)[-2:]}"
