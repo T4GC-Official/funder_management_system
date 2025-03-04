9ac9b216360e6d8308e568ed19d259ac20113e23
ajith4Tech <ajith@tech4goodcommunity.com>
2025-03-04 16:27:25 +0530

    Mod[Organisation POC, POC assigned]: Added Validation for Phone Number.

--- funder_management_system/funder_management_system/doctype/organisation_poc/organisation_poc.js
+++ funder_management_system/funder_management_system/doctype/organisation_poc/organisation_poc.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Tech4Good Community and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Organisation POC", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Organisation POC", {
+	validate: function (frm) {
+        if(frm.doc.phone){
+            phone = frm.doc.phone.slice(-10);
+            if(phone.length < 10){
+                frappe.msgprint("Phone number is invalid");
+                frappe.validated = false;
+            }
+            
+        }  
+    },
+});
