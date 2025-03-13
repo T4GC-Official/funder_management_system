59139dc270f9f4b57c776c7de94fa2194804e364
ajith4Tech <ajith@tech4goodcommunity.com>
2025-03-13 19:20:30 +0530

    WIP testing enabling moved the currency related hook drom after install to before install

--- funder_management_system/hooks.py
+++ funder_management_system/hooks.py
@@ -99,8 +99,8 @@ app_include_js = "/assets/funder_management_system/js/custom_toolbar.js"
 # Installation
 # ------------
 
-# before_install = "funder_management_system.install.before_install"
-after_install = ["funder_management_system.utils.create_financial_year", "funder_management_system.utils.set_currency_permission"]
+before_install = ["funder_management_system.utils.set_currency_permission"]
+after_install = ["funder_management_system.utils.create_financial_year"]
 
 # Uninstallation
 # ------------
