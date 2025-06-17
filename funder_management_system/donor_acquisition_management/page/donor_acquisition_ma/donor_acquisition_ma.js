145de8b3aaa427b492a78b10962b5932d98f57c1
neuron <praveen.902012@gmail.com>
2025-06-18 04:32:13 +0530

    added chart title when no data is there

--- funder_management_system/donor_acquisition_management/page/donor_acquisition_ma/donor_acquisition_ma.js
+++ funder_management_system/donor_acquisition_management/page/donor_acquisition_ma/donor_acquisition_ma.js
@@ -181,12 +181,7 @@ frappe.pages['donor-acquisition-ma'].on_page_load = function(wrapper) {
 							colors: config.chartColors
 						});
 					} else {
-						el.innerHTML = `
-						<div class="col-md-6 mb-4">
-					<div id="${chartId}" class="dashboard-chart-box">Data not available for ${config.chartTitle}</div>
-				</div>
-						
-						`;
+						el.innerHTML = `<p class="text-muted">No data available  ${config.chartTitle}.</p>`;
 					}
 				}
 			});
